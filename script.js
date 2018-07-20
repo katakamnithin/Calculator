feeadbb9dc105befc2bb574f31fc329696ac6633
katakamnithin <41443685+katakamnithin@users.noreply.github.com>
2018-07-20 15:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+<script type="text/javascript" >
+var disp = function display(x) ;{
+  Calculator.view.value=Calculator.view.value+x
+}
+</script>
+
+/*document.getElementById("1");
+
+		var one = document.getElementById('1');
+		if(one){
+/*		one.addEventListener('click',function(){
+			Calculator.view.value=Calculator.view.value+1;
+			console.log(Calculator);
+		};
+    }*/
+
